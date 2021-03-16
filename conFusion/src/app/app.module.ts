3a5662494612cd7a7e76a29d2aed5a0709eefe60
Bozlur Rahman <bozlurrahman.cmt@gmail.com>
2021-03-16 13:59:16 +0600

    ng generate component about ng generate component home ng generate component contact ng generate module app-routing

--- conFusion/src/app/app.module.ts
+++ conFusion/src/app/app.module.ts
@@ -17,6 +17,9 @@ import { MenuComponent } from './menu/menu.component';
 import { DishdetailComponent } from './dishdetail/dishdetail.component';
 import { HeaderComponent } from './header/header.component';
 import { FooterComponent } from './footer/footer.component';
+import { AboutComponent } from './about/about.component';
+import { HomeComponent } from './home/home.component';
+import { ContactComponent } from './contact/contact.component';
  
 @NgModule({
   declarations: [
@@ -24,7 +27,10 @@ import { FooterComponent } from './footer/footer.component';
     MenuComponent,
     DishdetailComponent,
     HeaderComponent,
-    FooterComponent
+    FooterComponent,
+    AboutComponent,
+    HomeComponent,
+    ContactComponent
   ],
   imports: [
     BrowserModule,
