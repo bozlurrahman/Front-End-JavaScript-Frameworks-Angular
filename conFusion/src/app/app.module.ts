e22bcf5515a0e242a6ab2ee5f3aa567b911a1585
Bozlur Rahman <bozlurrahman.cmt@gmail.com>
2021-03-23 10:21:10 +0600

    ng g directive directives/highlight

--- conFusion/src/app/app.module.ts
+++ conFusion/src/app/app.module.ts
@@ -36,6 +36,7 @@ import { HomeComponent } from './home/home.component';
 import { ContactComponent } from './contact/contact.component';
 import { LoginComponent } from './login/login.component';
 import { baseURL } from './shared/baseurl';
+import { HighlightDirective } from './directives/highlight.directive';
 
 @NgModule({
   declarations: [
@@ -47,7 +48,8 @@ import { baseURL } from './shared/baseurl';
     AboutComponent,
     HomeComponent,
     ContactComponent,
-    LoginComponent
+    LoginComponent,
+    HighlightDirective
   ],
   imports: [
     AppRoutingModule,
