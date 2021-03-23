6b2771aef91b7a3cc6ad6ee2882a1df6129134cc
Bozlur Rahman <bozlurrahman.cmt@gmail.com>
2021-03-23 09:53:14 +0600

    HTTP Part 2

--- conFusion/src/app/app.module.ts
+++ conFusion/src/app/app.module.ts
@@ -24,6 +24,7 @@ import { AppComponent } from './app.component';
 import { DishService } from './services/dish.service';
 import { PromotionService } from './services/promotion.service';
 import { LeaderService } from './services/leader.service';
+import { ProcessHTTPMsgService } from './services/process-httpmsg.service';
 
 import 'hammerjs';
 import { MenuComponent } from './menu/menu.component';
@@ -74,6 +75,7 @@ import { baseURL } from './shared/baseurl';
     DishService, 
     PromotionService,
     LeaderService,
+    ProcessHTTPMsgService,
     {provide: 'BaseURL', useValue: baseURL}
   ],
   entryComponents: [
