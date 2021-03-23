0e787fdd4f50a0a55a9fac950a615cd11edfe298
Bozlur Rahman <bozlurrahman.cmt@gmail.com>
2021-03-23 16:43:02 +0600

    Component Test. run by-> $ng test

--- conFusion/src/app/menu/menu.component.spec.ts
+++ conFusion/src/app/menu/menu.component.spec.ts
@@ -9,8 +9,12 @@ import { DishService } from '../services/dish.service';
 import { DISHES } from '../shared/dishes';
 import { baseURL } from '../shared/baseurl';
 import { Observable, of } from 'rxjs';
+import { By } from '@angular/platform-browser';
+import { DebugElement } from '@angular/core';
 import { MatGridListModule } from '@angular/material/grid-list';
 import { MatProgressSpinnerModule } from '@angular/material/progress-spinner';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
+
 describe('MenuComponent', () => {
   let component: MenuComponent;
   let fixture: ComponentFixture<MenuComponent>;
@@ -20,6 +24,8 @@ describe('MenuComponent', () => {
     const dishServiceStub = {
       getDishes: function(): Observable<Dish[]> {
         return of(DISHES);
+        // let http: HttpClient
+        // return http.get<Dish[]>(baseURL + 'dishes');
       }
     };
 
@@ -52,5 +58,21 @@ describe('MenuComponent', () => {
     expect(component).toBeTruthy();
   });
 
+  it('dishes items should be 4', () => {
+    expect(component.dishes.length).toBe(4);
+    expect(component.dishes[1].name).toBe('Zucchipakoda');
+    expect(component.dishes[3].featured).toBeFalsy();
+  });
+  
+  it('should use dishes in the template', () => {
+    fixture.detectChanges();
 
+    let de:      DebugElement;
+    let el:      HTMLElement;
+    de = fixture.debugElement.query(By.css('h1'));
+    el = de.nativeElement;
+    
+    expect(el.textContent).toContain(DISHES[0].name.toUpperCase());
+
+  });
 });
